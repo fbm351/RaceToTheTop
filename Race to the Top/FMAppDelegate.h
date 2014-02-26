eb6dbfd1d80cefe4d3e7f8c2bf99ac30440a62aa
Fred Myers <fred@fred-myers.com>
2014-02-26 14:12:17 -0500

    Initial Commit

---
+++ Race to the Top/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  Race to the Top
+//
+//  Created by Fredrick Myers on 2/26/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Race to the Top/FMViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMViewController.h
+//  Race to the Top
+//
+//  Created by Fredrick Myers on 2/26/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMViewController : UIViewController
+
+@end
